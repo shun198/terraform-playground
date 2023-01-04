b913d884cd1e75ca5de1e54bc659578dd760aadc
shun198 <shunhiroseluvmri@gmail.com>
2023-01-05 05:39:07 +0900

    public_bを追加

--- infra/network.tf
+++ infra/network.tf
@@ -28,7 +28,8 @@ resource "aws_internet_gateway" "main" {
 # Public Subnet Configuration
 # ------------------------------
 resource "aws_subnet" "public_a" {
-  cidr_block              = "10.0.1.0/24"
+  cidr_block = "10.0.1.0/24"
+  # サブネットに配置されたインスタンスにパブリックIPアドレスが付与される
   map_public_ip_on_launch = true
   vpc_id                  = aws_vpc.main.id
   availability_zone       = "${data.aws_region.current.name}a"
@@ -36,6 +37,104 @@ resource "aws_subnet" "public_a" {
   tags = merge(
     local.common_tags,
     tomap({ "Name" = "${local.prefix}-public-a" })
+  )
+}
+
+# ルートテーブルの設定
+resource "aws_route_table" "public_a" {
+  vpc_id = aws_vpc.main
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-public-a" })
+  )
+}
+
+# ルートテーブルをパブリックサブネットaと紐付ける
+# タグをサポートしてないのでつけない
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public_a.id
+}
+
+# IGWへのルーティングを設定
+# タグをサポートしてないのでつけない
+resource "aws_route" "public_internet_access_a" {
+  route_table_id = aws_route_table.public_a.id
+  # インターネット(0.0.0.0/0)へのアクセスを許可
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+# ElasticIPをpublic_a内に作成
+resource "aws_eip" "public_a" {
+  vpc = true
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-public-a" })
+  )
+}
+
+# NATゲートウェイ
+resource "aws_nat_gateway" "public_a" {
+  allocation_id = aws_eip.public_a.id
+  subnet_id     = aws_subnet.public_a.id
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-public-a" })
+  )
+}
+
+resource "aws_subnet" "public_b" {
+  cidr_block = "10.0.2.0/24"
+  # サブネットに配置されたインスタンスにパブリックIPアドレスが付与される
+  map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.main.id
+  availability_zone       = "${data.aws_region.current.name}b"
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-public-b" })
+  )
+}
+resource "aws_route_table" "public_b" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-public-b" })
+  )
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public_b.id
+}
+
+resource "aws_route" "public_internet_access_b" {
+  route_table_id         = aws_route_table.public_b.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_eip" "public_b" {
+  vpc = true
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-public-b" })
+  )
+}
+
+resource "aws_nat_gateway" "public_b" {
+  allocation_id = aws_eip.public_b
+  subnet_id     = aws_subnet.public_b.id
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-public-b" })
   )
 }
 # ------------------------------

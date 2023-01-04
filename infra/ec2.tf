4fbb9763907fec4aa5f1f2392ad145b51f4112f2
shun198 <shunhiroseluvmri@gmail.com>
2023-01-04 21:37:46 +0900

    パブリックサブネットを追加

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -5,10 +5,11 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0bba69335379e17f8"
   instance_type = "t2.micro"
 
-  # mergeを使ってtagをoverrideする
+  # tagをoverrideしたいときはmergeを使う
+  # 今回はlocal.common_tagsにNameタグを追加する形でoverrideする
   tags = merge(
     # common_tagとnameを合体させてつける
     local.common_tags,
-    map("Name", "${local.prefix}-ExampleAppServerInstance")
+    tomap({ "Name" = "${local.prefix}-ec2" })
   )
 }
--- infra/network.tf
+++ infra/network.tf
@@ -6,15 +6,38 @@ resource "aws_vpc" "main" {
   enable_dns_support   = true
   enable_dns_hostnames = true
 
-  tags = {
-    Name = "${var.project}-vpc"
-  }
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-vpc" })
+  )
+}
+
+# ------------------------------
+# Internert Gateway Configuration
+# ------------------------------
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-igw" })
+  )
 }
 
 # ------------------------------
 # Public Subnet Configuration
 # ------------------------------
+resource "aws_subnet" "public_a" {
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.main.id
+  availability_zone       = "${data.aws_region.current.name}a"
 
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-public-a" })
+  )
+}
 # ------------------------------
 # Private Subnet Configuration
 # ------------------------------

c1afaf27f9d3b8cb08caaaf132f3c799aa83999d
shun198 <shunhiroseluvmri@gmail.com>
2023-07-15 19:44:02 +0900

    👍 update cloudwatch configuration

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -20,6 +20,25 @@ resource "aws_cloudwatch_log_group" "ecs_task_logs" {
   )
 }
 
+data "template_file" "app_container_definitions" {
+  template = file("./templates/ecs/container-definitions.json.tpl")
+
+  vars = {
+    app_image                = var.ecr_image_app
+    proxy_image              = var.ecr_image_proxy
+    django_secret_key        = var.django_secret_key
+    db_host                  = aws_db_instance.main.address
+    db_name                  = aws_db_instance.main.name
+    db_user                  = aws_db_instance.main.username
+    db_pass                  = aws_db_instance.main.password
+    log_group_name           = aws_cloudwatch_log_group.ecs_task_logs.name
+    log_group_region         = data.aws_region.current.name
+    allowed_hosts            = aws_route53_record.app.fqdn
+    s3_storage_bucket_name   = aws_s3_bucket.app_public_files.bucket
+    s3_storage_bucket_region = data.aws_region.current.name
+  }
+}
+
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_task_definition.html#example-usage
 resource "aws_ecs_task_definition" "app" {
   family                   = "${local.prefix}-app"

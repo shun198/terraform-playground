ac55c8009e20ca031fb4cf947d45cdeabb37b355
shun198 <shunhiroseluvmri@gmail.com>
2023-07-15 20:34:11 +0900

    👍 add ssm.tf

---
+++ infra/ssm.tf
@@ -0,0 +1,15 @@
+resource "aws_ssm_parameter" "SECRET_KEY" {
+  name        = "SECRET_KEY"
+  description = "Secret Key for Django Application"
+  type        = "SecureString"
+  value       = "test"
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}_SECRET_KEY" })
+  )
+}
+
+output "SECRET_KEY_VALUE" {
+  value = aws_ssm_parameter.SECRET_KEY.value
+}

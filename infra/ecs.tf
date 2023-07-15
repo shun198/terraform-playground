1d17812749b7eeb8048d11b785b7f756420383b4
shun198 <shunhiroseluvmri@gmail.com>
2023-07-15 19:33:46 +0900

    👍 update taskdef

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -10,9 +10,20 @@ resource "aws_ecs_cluster" "main" {
   )
 }
 
-resource "aws_ecs_task_definition" "api" {
-  family                   = "${local.prefix}-api"
-  container_definitions    = data.template_file.api_container_definitions.rendered
+# ECS内のCloudWatchの設定
+resource "aws_cloudwatch_log_group" "ecs_task_logs" {
+  name = "${local.prefix}-app"
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-ecs-cloudwatch-logs" })
+  )
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_task_definition.html#example-usage
+resource "aws_ecs_task_definition" "app" {
+  family                   = "${local.prefix}-app"
+  container_definitions    = data.template_file.app_container_definitions.rendered
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = 256
@@ -43,7 +54,7 @@ resource "aws_security_group" "ecs_service" {
     protocol  = "tcp"
     cidr_blocks = [
       aws_subnet.private_a.cidr_block,
-      aws_subnet.private_b.cidr_block,
+      aws_subnet.private_c.cidr_block,
     ]
   }
 
@@ -59,10 +70,10 @@ resource "aws_security_group" "ecs_service" {
   tags = local.common_tags
 }
 
-resource "aws_ecs_service" "api" {
-  name             = "${local.prefix}-api"
+resource "aws_ecs_service" "app" {
+  name             = "${local.prefix}-app"
   cluster          = aws_ecs_cluster.main.name
-  task_definition  = aws_ecs_task_definition.api.family
+  task_definition  = aws_ecs_task_definition.app.family
   desired_count    = 1
   launch_type      = "FARGATE"
   platform_version = "1.4.0"
@@ -70,17 +81,16 @@ resource "aws_ecs_service" "api" {
   network_configuration {
     subnets = [
       aws_subnet.private_a.id,
-      aws_subnet.private_b.id,
+      aws_subnet.private_c.id,
     ]
     security_groups = [aws_security_group.ecs_service.id]
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.api.arn
+    target_group_arn = aws_lb_target_group.app.arn
     container_name   = "proxy"
     container_port   = 8000
   }
 
-  depends_on = [aws_lb_listener.api_https]
+  depends_on = [aws_lb_listener.app_https]
 }
-

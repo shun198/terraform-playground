c8c002b0e192696f5b7420ca1cbaea0a622022a0
shun198 <shunhiroseluvmri@gmail.com>
2023-07-15 17:32:43 +0900

    👍 add outputs

--- infra/database.tf
+++ infra/database.tf
@@ -21,10 +21,13 @@ resource "aws_security_group" "rds" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    protocol    = "tcp"
-    from_port   = 5432
-    to_port     = 5432
-    cidr_blocks = ["10.0.0.0/16"]
+    protocol  = "tcp"
+    from_port = 5432
+    to_port   = 5432
+    security_groups = [
+      aws_security_group.bastion.id,
+    ]
+
   }
 
   egress {
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,6 +1,10 @@
 # ------------------------------
 # Configure DB Output
 # ------------------------------
-# output "db_host" {
-#   value = aws_db_instance.main.address
-# }
+output "db_host" {
+  value = aws_db_instance.main.address
+}
+
+output "bastion_host" {
+  value = aws_instance.bastion.public_dns
+}
